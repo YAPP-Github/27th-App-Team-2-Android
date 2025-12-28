4ce92ad222c12ecc80caeb4c85ac23dcd587012c
Ojongseok <whdtjr6889@naver.com>
2025-12-29 08:41:43 +0900

    [chore] DataStoreRepository.kt 및 DataStoreModule.kt 정의

--- core/data/src/main/java/com/neki/android/core/data/repository/di/RepositoryModule.kt
+++ core/data/src/main/java/com/neki/android/core/data/repository/di/RepositoryModule.kt
@@ -1,20 +1,30 @@
 package com.neki.android.core.data.repository.di
 
+import com.neki.android.core.data.repository.impl.DataStoreRepositoryImpl
 import com.neki.android.core.data.repository.impl.SampleRepositoryImpl
+import com.neki.android.core.dataapi.repository.DataStoreRepository
 import com.neki.android.core.dataapi.repository.SampleRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 internal abstract class RepositoryModule {
 
     @Binds
+    @Singleton
     abstract fun bindSampleRepositoryImpl(
         sampleRepositoryImpl: SampleRepositoryImpl
     ): SampleRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindDataStoreRepositoryImpl(
+        dataStoreRepositoryImpl: DataStoreRepositoryImpl
+    ): DataStoreRepository
 
 
 }
