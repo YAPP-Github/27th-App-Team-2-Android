5a582808883338f79bf2ee62f648a428815af50d
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-12-30 02:54:46 +0900

    [feat] #3: NavDisplay 추가

--- app/src/main/java/com/neki/android/app/MainActivity.kt
+++ app/src/main/java/com/neki/android/app/MainActivity.kt
@@ -11,11 +11,23 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation3.runtime.entryProvider
+import androidx.navigation3.ui.NavDisplay
 import com.neki.android.core.designsystem.ui.theme.NekiTheme
+import com.neki.android.core.navigation.EntryProviderInstaller
+import com.neki.android.core.navigation.NavigatorImpl
+import com.neki.android.core.navigation.toEntries
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var navigator: NavigatorImpl
+
+    @Inject
+    lateinit var entryProviderScopes: Set<@JvmSuppressWildcards EntryProviderInstaller>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,28 +35,17 @@ class MainActivity : ComponentActivity() {
         setContent {
             NekiTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    NavDisplay(
+                        modifier = Modifier.padding(innerPadding),
+                        entries = navigator.state.toEntries(
+                            entryProvider = entryProvider {
+                                entryProviderScopes.forEach { builder -> this.builder() }
+                            },
+                        ),
+                        onBack = { navigator.goBack() },
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NekiTheme {
-        Greeting("Android")
     }
 }
