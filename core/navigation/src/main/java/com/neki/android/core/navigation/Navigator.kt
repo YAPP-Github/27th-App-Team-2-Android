5f8c58cfff1d35cf5c41c794ed98dff9d6ddf45e
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-12-30 01:44:47 +0900

    [feat] #3: EntryProviderInstaller 타입 추가

--- core/navigation/src/main/java/com/neki/android/core/navigation/Navigator.kt
+++ core/navigation/src/main/java/com/neki/android/core/navigation/Navigator.kt
@@ -1,6 +1,9 @@
 package com.neki.android.core.navigation
 
+import androidx.navigation3.runtime.EntryProviderScope
 import androidx.navigation3.runtime.NavKey
+
+typealias EntryProviderInstaller = EntryProviderScope<NavKey>.() -> Unit
 
 interface Navigator {
     fun navigate(key: NavKey)
