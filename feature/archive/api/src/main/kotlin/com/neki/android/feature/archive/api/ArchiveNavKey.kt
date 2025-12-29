99d6084096dcc0b92831f60c9ec9e3f101dd435b
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-12-30 01:51:55 +0900

    [feat] #3: 아카이빙 NavKey, 네비게이션 이벤트 구현

---
+++ feature/archive/api/src/main/kotlin/com/neki/android/feature/archive/api/ArchiveNavKey.kt
@@ -0,0 +1,15 @@
+package com.neki.android.feature.archive.com.neki.android.feature.archive.api
+
+import androidx.navigation3.runtime.NavKey
+import com.neki.android.core.navigation.Navigator
+import kotlinx.serialization.Serializable
+
+sealed interface ArchiveNavKey : NavKey {
+
+    @Serializable
+    data object Archive : ArchiveNavKey
+}
+
+fun Navigator.navigateToArchive() {
+    navigate(ArchiveNavKey.Archive)
+}
