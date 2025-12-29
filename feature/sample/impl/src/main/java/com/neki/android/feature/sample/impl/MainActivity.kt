9bc217eaabb5a0e2ec7d6bc8c49aebad46073e06
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-12-29 15:52:43 +0900

    [chore] #3: MainViewModel 클래스 제거

--- feature/sample/impl/src/main/java/com/neki/android/feature/sample/impl/MainActivity.kt
+++ feature/sample/impl/src/main/java/com/neki/android/feature/sample/impl/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -17,7 +16,6 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,8 +30,6 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-
-        viewModel
     }
 }
 
