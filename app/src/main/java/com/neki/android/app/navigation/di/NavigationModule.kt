75380949e7f57348daf5b27c04ea682348da1c1c
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2026-01-01 00:50:35 +0900

    [feat] #3: NavigationState 에 ActivityRetainedScoped 적용

--- app/src/main/java/com/neki/android/app/navigation/di/NavigationModule.kt
+++ app/src/main/java/com/neki/android/app/navigation/di/NavigationModule.kt
@@ -7,12 +7,14 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityRetainedComponent
+import dagger.hilt.android.scopes.ActivityRetainedScoped
 
 @Module
 @InstallIn(ActivityRetainedComponent::class)
 internal object NavigationModule {
 
     @Provides
+    @ActivityRetainedScoped
     fun providesNavigationState(): NavigationState {
         return NavigationState(
             startKey = START_NAV_KEY,
