182f30552a947cd73933b305af359577334ec1c4
Ojongseok <whdtjr6889@naver.com>
2026-01-04 01:38:36 +0900

    [chore] #4 : SampleRepository, SampleRepositoryImpl 제거

--- core/data/src/main/java/com/neki/android/core/data/repository/di/RepositoryModule.kt
+++ core/data/src/main/java/com/neki/android/core/data/repository/di/RepositoryModule.kt
@@ -1,9 +1,7 @@
 package com.neki.android.core.data.repository.di
 
 import com.neki.android.core.data.repository.impl.DataStoreRepositoryImpl
-import com.neki.android.core.data.repository.impl.SampleRepositoryImpl
 import com.neki.android.core.dataapi.repository.DataStoreRepository
-import com.neki.android.core.dataapi.repository.SampleRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -16,13 +14,8 @@ internal interface RepositoryModule {
 
     @Binds
     @Singleton
-    fun bindSampleRepositoryImpl(
-        sampleRepositoryImpl: SampleRepositoryImpl,
-    ): SampleRepository
-
-    @Binds
-    @Singleton
     fun bindDataStoreRepositoryImpl(
         dataStoreRepositoryImpl: DataStoreRepositoryImpl,
     ): DataStoreRepository
+
 }
