4d8e81ec0d3ef6c7dfa60ec2abbfcf0ef6eba1ba
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2026-01-05 16:18:52 +0900

    [feat] #10: CameraX, MLKit 바코드 스캔 의존성 추가

--- feature/photo-upload/impl/build.gradle.kts
+++ feature/photo-upload/impl/build.gradle.kts
@@ -8,4 +8,12 @@ android {
 
 dependencies {
     implementation(projects.feature.photoUpload.api)
+
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.mlkit.barcode.scanning)
+
+    implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.mlkit.vision)
+    implementation(libs.androidx.camera.compose)
 }
