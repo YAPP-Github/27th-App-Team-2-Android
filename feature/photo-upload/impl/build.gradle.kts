39262c331e05e4855c486448ebd0333a3f4a3565
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2026-01-07 00:03:30 +0900

    [feat] #10: 포토이즘 URL, JPG 추가

--- feature/photo-upload/impl/build.gradle.kts
+++ feature/photo-upload/impl/build.gradle.kts
@@ -1,9 +1,28 @@
+import java.util.Properties
+import kotlin.apply
+
 plugins {
     alias(libs.plugins.neki.android.feature.impl)
 }
 
+val localPropertiesFile = project.rootProject.file("local.properties")
+val properties = Properties().apply {
+    if (localPropertiesFile.exists()) {
+        load(localPropertiesFile.inputStream())
+    }
+}
+
 android {
     namespace = "com.neki.android.feature.photo_upload.impl"
+
+    buildFeatures {
+        buildConfig = true
+    }
+
+    defaultConfig {
+        buildConfigField("String", "PHOTOISM_URL", properties["PHOTOISM_URL"].toString())
+        buildConfigField("String", "PHOTOISM_IMG_URL_END", properties["PHOTOISM_IMG_URL_END"].toString())
+    }
 }
 
 dependencies {
