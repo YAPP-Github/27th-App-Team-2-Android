6d8507f746ce4fc6958d444a62563161764e96b1
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2026-01-07 00:04:15 +0900

    [feat] #10: 이미지 파싱용 WebClient 임시 구현

--- feature/photo-upload/impl/src/main/java/com/neki/android/feature/photo_upload/impl/qrscan/util/PhotoWebViewClient.kt
+++ feature/photo-upload/impl/src/main/java/com/neki/android/feature/photo_upload/impl/qrscan/util/PhotoWebViewClient.kt
@@ -1,7 +1,26 @@
 package com.neki.android.feature.photo_upload.impl.qrscan.util
 
+import android.webkit.WebResourceRequest
+import android.webkit.WebResourceResponse
+import android.webkit.WebView
 import android.webkit.WebViewClient
+import com.neki.android.feature.photo_upload.impl.BuildConfig
 
 class PhotoWebViewClient(
     private val onImageUrlDetected: (String) -> Unit,
-) : WebViewClient() {}
+) : WebViewClient() {
+
+    override fun shouldInterceptRequest(
+        view: WebView?,
+        request: WebResourceRequest?,
+    ): WebResourceResponse? {
+        val url = request?.url.toString()
+
+        // Photoism
+        if (url.contains(BuildConfig.PHOTOISM_URL) && url.endsWith(BuildConfig.PHOTOISM_IMG_URL_END)) {
+            onImageUrlDetected(url)
+        }
+
+        return super.shouldInterceptRequest(view, request)
+    }
+}
