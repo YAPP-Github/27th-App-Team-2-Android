95c12361326b00f048b551906fd962e4d008d29d
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2026-01-10 04:07:05 +0900

    [chore] #15: 불필요한 import 제거

--- core/designsystem/src/main/java/com/neki/android/core/designsystem/topbar/NekiTopBar.kt
+++ core/designsystem/src/main/java/com/neki/android/core/designsystem/topbar/NekiTopBar.kt
@@ -2,7 +2,6 @@ package com.neki.android.core.designsystem.topbar
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
