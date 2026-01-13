c0df5e73f5e431fc60f0301a5fba66a59417637a
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2026-01-13 23:40:39 +0900

    [feat] #18: PoseContract 정의
    포즈피드 화면에서 사용될 `PoseState`, `PoseIntent`, `PoseEffect`를 정의합니다. 또한, 인원수를 나타내는 `NumberOfPeople` enum 클래스를 추가합니다.

---
+++ feature/pose/impl/src/main/java/com/neki/android/feature/pose/impl/PoseContract.kt
@@ -0,0 +1,41 @@
+package com.neki.android.feature.pose.impl
+
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
+
+data class PoseState(
+    val isLoading: Boolean = false,
+    val selectedNumberOfPeople: NumberOfPeople = NumberOfPeople.UNSELECTED,
+    val showScrappedPose: Boolean = false,
+    val randomPoseList: ImmutableList<String> = persistentListOf(),
+    val scrappedPosLise: ImmutableList<String> = persistentListOf(),
+    val selectedPose: String = "",
+)
+
+sealed interface PoseIntent {
+    // Pose Main
+    data object ClickAlarmIcon : PoseIntent
+    data object ClickNumberOfPeople : PoseIntent
+    data object ClickScrap : PoseIntent
+    data class ClickPoseItem(val id: Long) : PoseIntent
+    data object ClickRandomPoseRecommendation : PoseIntent
+    data class ClickNumberOfPeopleSheetItem(val numberOfPeople: NumberOfPeople) : PoseIntent
+
+    // Pose Detail
+    data object ClickBackIcon : PoseIntent
+    data object ClickScrapIcon : PoseIntent
+}
+
+sealed interface PoseEffect {
+    data class ShowToast(val message: String) : PoseEffect
+    data class NavigateToPoseDetail(val id: Long) : PoseEffect
+}
+
+enum class NumberOfPeople(val displayText: String, val value: Int) {
+    UNSELECTED("인원수", 0),
+    ONE("1인", 1),
+    TWO("2인", 2),
+    THREE("3인", 3),
+    FOUR("4인", 4),
+    FIVE_OR_MORE("5인 이상", 5),
+}
