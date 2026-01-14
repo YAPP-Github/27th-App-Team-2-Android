5c07395dce3f03200af5ef08c35ed0169b054215
Ojongseok <whdtjr6889@naver.com>
2026-01-15 03:56:44 +0900

    [chore] #13 시스템 상단바 라이트 모드로 설정

--- core/designsystem/src/main/java/com/neki/android/core/designsystem/ui/theme/Theme.kt
+++ core/designsystem/src/main/java/com/neki/android/core/designsystem/ui/theme/Theme.kt
@@ -1,10 +1,14 @@
 package com.neki.android.core.designsystem.ui.theme
 
+import android.app.Activity
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 @Composable
 fun ProvideTypographyAndColor(
@@ -23,6 +27,14 @@ fun ProvideTypographyAndColor(
 fun NekiTheme(
     content: @Composable () -> Unit,
 ) {
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = true
+        }
+    }
+
     ProvideTypographyAndColor(
         typography = defaultNekiTypography,
         colors = defaultNekiColors,
