f9a720506e3d1122646b7d75649157ed99f32c4e
Ojongseok <whdtjr6889@naver.com>
2026-01-17 23:40:48 +0900

    [feat] #13 DirectionHelper.kt 클래스 정의

---
+++ feature/map/impl/src/main/java/com/neki/android/feature/map/impl/util/DirectionHelper.kt
@@ -0,0 +1,29 @@
+package com.neki.android.feature.map.impl.util
+
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.core.net.toUri
+
+class DirectionHelper() {
+    fun moveAppOrStore(
+        context: Context,
+        url: String,
+        packageName: String,
+    ) {
+        val intent = if (isInstalled(context.packageManager, packageName)) {
+            Intent(Intent.ACTION_VIEW, url.toUri())
+        } else {
+            Intent(
+                Intent.ACTION_VIEW,
+                "market://details?id=$packageName".toUri(),
+            )
+        }
+        context.startActivity(intent)
+    }
+
+    private fun isInstalled(
+        packageManager: PackageManager,
+        packageName: String,
+    ): Boolean = runCatching { packageManager.getPackageInfo(packageName, 0) }.isSuccess
+}
