8df32fd288e2b20cf99406b6c4568afd8c47cc1f
Ojongseok <whdtjr6889@naver.com>
2026-01-18 01:03:49 +0900

    [feat] #13 길찾기 앱 enum class 내 packageName 필드 추가

--- feature/map/impl/src/main/java/com/neki/android/feature/map/impl/const/DirectionAppConst.kt
+++ feature/map/impl/src/main/java/com/neki/android/feature/map/impl/const/DirectionAppConst.kt
@@ -6,17 +6,21 @@ import com.neki.android.core.designsystem.R
 enum class DirectionAppConst(
     @DrawableRes val appLogoRes: Int,
     val appName: String,
+    val packageName: String,
 ) {
     GOOGLE_MAP(
         appLogoRes = R.drawable.icon_google_map,
         appName = "구글맵",
+        packageName = "com.google.android.apps.maps",
     ),
     NAVER_MAP(
         appLogoRes = R.drawable.icon_naver_map,
         appName = "네이버 지도",
+        packageName = "com.nhn.android.nmap",
     ),
     KAKAO_MAP(
         appLogoRes = R.drawable.icon_kakao_map,
         appName = "카카오맵",
+        packageName = "net.daum.android.map",
     ),
 }
