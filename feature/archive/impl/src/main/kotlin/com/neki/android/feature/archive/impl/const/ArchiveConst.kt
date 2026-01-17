e04b9ec25541e1778c00dd66e9966d28b663337e
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2026-01-18 02:36:15 +0900

    [chore] #28: 아카이브 행의 텍스트 버튼 패딩 8에서 12로 수정

--- feature/archive/impl/src/main/kotlin/com/neki/android/feature/archive/impl/const/ArchiveConst.kt
+++ feature/archive/impl/src/main/kotlin/com/neki/android/feature/archive/impl/const/ArchiveConst.kt
@@ -1,7 +1,7 @@
 package com.neki.android.feature.archive.impl.const
 
 internal object ArchiveConst {
-    internal const val ARCHIVE_ROW_TEXT_BUTTON_PADDING = 8
+    internal const val ARCHIVE_ROW_TEXT_BUTTON_PADDING = 12
     internal const val ARCHIVE_LAYOUT_HORIZONTAL_PADDING = 20
     internal const val ARCHIVE_LAYOUT_BOTTOM_PADDING = 76
 }
