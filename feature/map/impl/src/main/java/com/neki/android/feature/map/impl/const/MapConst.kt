b0a0fef698d05dd3fb95426dcdf987fed62a1052
Ojongseok <whdtjr6889@naver.com>
2026-01-20 00:53:55 +0900

    [feat] #13 MapConst 상수 정의

---
+++ feature/map/impl/src/main/java/com/neki/android/feature/map/impl/const/MapConst.kt
@@ -0,0 +1,20 @@
+package com.neki.android.feature.map.impl.const
+
+internal object MapConst {
+    internal const val BOTTOM_NAVIGATION_BAR_HEIGHT = 72
+
+    internal const val MARKER_BACKGROUND_SIZE = 54
+    internal const val MARKER_BACKGROUND_RADIUS = 20
+    internal const val MARKER_IMAGE_SIZE = 50
+    internal const val MARKER_IMAGE_RADIUS = 18
+    internal const val MARKER_PADDING = 2
+    internal const val FOCUSED_MARKER_BACKGROUND_SIZE = 72
+    internal const val FOCUSED_MARKER_BACKGROUND_RADIUS = 26
+    internal const val FOCUSED_MARKER_IMAGE_SIZE = 60
+    internal const val FOCUSED_MARKER_IMAGE_RADIUS = 21
+    internal const val FOCUSED_MARKER_PADDING = 6
+    internal const val MARKER_TRIANGLE_SIZE = 12
+
+
+
+}
