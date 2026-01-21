2856b7b0224752cccdc0dc541107273c3fc47e7a
Ojongseok <whdtjr6889@naver.com>
2026-01-22 00:28:25 +0900

    [feat] #35 마이페이지 컴포넌트 및 메인화면 UI 구성

--- feature/mypage/impl/src/main/java/com/neki/android/feature/mypage/impl/MyPageScreen.kt
+++ feature/mypage/impl/src/main/java/com/neki/android/feature/mypage/impl/MyPageScreen.kt
@@ -1,13 +1,24 @@
 package com.neki.android.feature.mypage.impl
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.neki.android.core.designsystem.ComponentPreview
 import com.neki.android.core.designsystem.ui.theme.NekiTheme
+import com.neki.android.feature.mypage.impl.component.MainTopBar
+import com.neki.android.feature.mypage.impl.component.ProfileCard
+import com.neki.android.feature.mypage.impl.component.SectionItem
+import com.neki.android.feature.mypage.impl.component.SectionTitleText
 
 @Composable
 fun MyPageRoute(
@@ -20,15 +31,59 @@ fun MyPageRoute(
 fun MyPageScreen(
     modifier: Modifier = Modifier,
 ) {
-    Box(
-        modifier = modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center,
+    Column(
+        modifier = modifier
+            .fillMaxSize()
     ) {
-        Text(text = "마이페이지")
+        MainTopBar()
+        ProfileCard(
+            name = "오종석"
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(11.dp)
+                .background(color = NekiTheme.colorScheme.gray25)
+        )
+        Column {
+            SectionTitleText(
+                text = "권한 설정"
+            )
+            SectionItem(
+                text = "기기 권한"
+            )
+        }
+        Column {
+            SectionTitleText(
+                text = "서비스 정보 및 지원"
+            )
+            SectionItem(
+                text = "Neki에 문의하기"
+            )
+            SectionItem(
+                text = "이용약관"
+            )
+            SectionItem(
+                text = "개인정보 처리방침"
+            )
+            SectionItem(
+                text = "오픈소스 라이선스"
+            )
+            SectionItem(
+                text = "앱 버전 정보",
+                trailingContent = {
+                    Text(
+                        text = "v1.3.1",
+                        color = NekiTheme.colorScheme.gray500,
+                        style = NekiTheme.typography.body14Medium,
+                    )
+                },
+            )
+        }
     }
 }
 
-@Preview
+@ComponentPreview
 @Composable
 private fun MyPageScreenPreview() {
     NekiTheme {
---
+++ feature/mypage/impl/src/main/java/com/neki/android/feature/mypage/impl/component/PermissionSectionItem.kt
@@ -0,0 +1,93 @@
+package com.neki.android.feature.mypage.impl.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
+import com.neki.android.core.designsystem.ComponentPreview
+import com.neki.android.core.designsystem.R
+import com.neki.android.core.designsystem.extension.noRippleClickableSingle
+import com.neki.android.core.designsystem.ui.theme.NekiTheme
+
+@Composable
+fun PermissionSectionItem(
+    title: String,
+    subTitle: String,
+    isGranted: Boolean= false,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = {},
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .noRippleClickableSingle(onClick = onClick)
+            .padding(vertical = 12.dp, horizontal = 20.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            Text(
+                text = title,
+                color = NekiTheme.colorScheme.gray900,
+                style = NekiTheme.typography.body16Medium,
+            )
+            Text(
+                text = subTitle,
+                color = NekiTheme.colorScheme.gray400,
+                style = NekiTheme.typography.caption12Medium,
+            )
+        }
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+
+        ) {
+            Text(
+                text = if (isGranted) "허용됨" else "거부됨",
+                color = NekiTheme.colorScheme.gray500,
+                style = NekiTheme.typography.body14Medium,
+            )
+            Icon(
+                modifier = Modifier.size(16.dp),
+                imageVector = ImageVector.vectorResource(R.drawable.icon_arrow_right),
+                tint = NekiTheme.colorScheme.gray300,
+                contentDescription = null,
+            )
+        }
+    }
+}
+
+@ComponentPreview
+@Composable
+private fun PermissionSectionItemCameraPreview() {
+    NekiTheme {
+        PermissionSectionItem(
+            title = "카메라",
+            subTitle = "QR 촬영에 필요해요.",
+            isGranted = true,
+        )
+    }
+}
+
+@ComponentPreview
+@Composable
+private fun PermissionSectionItemLocationPreview() {
+    NekiTheme {
+        PermissionSectionItem(
+            title = "위치",
+            subTitle = "QR 촬영에 필요해요.",
+            isGranted = false
+        )
+    }
+}
