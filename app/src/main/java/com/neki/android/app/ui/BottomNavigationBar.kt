a2d0ae8f811288a2a37773752255052740cb595c
Ojongseok <whdtjr6889@naver.com>
2026-01-21 20:53:47 +0900

    [design] #35 BottomNavigationBar 디자인 변경사항 반영 및 tabbarShadow 확장함수 추가

--- app/src/main/java/com/neki/android/app/ui/BottomNavigationBar.kt
+++ app/src/main/java/com/neki/android/app/ui/BottomNavigationBar.kt
@@ -3,12 +3,14 @@ package com.neki.android.app.ui
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -26,6 +28,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation3.runtime.NavKey
 import com.neki.android.app.navigation.TopLevelNavItem
+import com.neki.android.core.designsystem.extension.tabbarShadow
 import com.neki.android.core.designsystem.ui.theme.NekiTheme
 
 @Composable
@@ -44,13 +47,15 @@ fun BottomNavigationBar(
         Surface(
             modifier = Modifier
                 .navigationBarsPadding()
-                .fillMaxWidth(),
+                .fillMaxWidth()
+                .tabbarShadow(),
+            color = NekiTheme.colorScheme.white,
         ) {
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(horizontal = 20.dp),
-                horizontalArrangement = Arrangement.spacedBy(2.5.dp),
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
             ) {
                 tabs.forEach { tab ->
                     BottomNavigationBarItem(
@@ -73,26 +78,29 @@ fun BottomNavigationBarItem(
     onClick: () -> Unit = {},
 ) {
     val icon = if (selected) tab.selectedIcon else tab.unselectedIcon
-    val color = if (selected) NekiTheme.colorScheme.gray800 else NekiTheme.colorScheme.gray100
+    val iconColor = if (selected) NekiTheme.colorScheme.gray800 else NekiTheme.colorScheme.gray200
+    val textColor = if (selected) NekiTheme.colorScheme.gray800 else NekiTheme.colorScheme.gray500
 
     Surface(
         modifier = modifier,
         onClick = onClick,
+        color = NekiTheme.colorScheme.white,
     ) {
         Column(
             modifier = Modifier.padding(vertical = 8.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(8.dp),
+            verticalArrangement = Arrangement.spacedBy(1.dp),
         ) {
             Icon(
+                modifier = Modifier.size(24.dp),
                 imageVector = ImageVector.vectorResource(icon),
                 contentDescription = stringResource(tab.iconTextId),
-                tint = color,
+                tint = iconColor,
             )
             Text(
                 text = stringResource(tab.iconTextId),
-                color = color,
-                style = NekiTheme.typography.caption12Medium,
+                color = textColor,
+                style = NekiTheme.typography.caption11SemiBold,
             )
         }
     }
