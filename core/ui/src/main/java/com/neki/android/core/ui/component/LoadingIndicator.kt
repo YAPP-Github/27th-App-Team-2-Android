35b941237de8a3c09da0f3fb2e26f3eeb504a86e
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2026-01-23 00:59:07 +0900

    [feat] #36: 로딩 인디케이터 UI 컴포넌트 추가
    `CircularProgressIndicator`를 `Dialog`로 감싼 `LoadingIndicator` 컴포저블을 추가합니다.

---
+++ core/ui/src/main/java/com/neki/android/core/ui/component/LoadingIndicator.kt
@@ -0,0 +1,42 @@
+package com.neki.android.core.ui.component
+
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import com.neki.android.core.designsystem.ComponentPreview
+import com.neki.android.core.designsystem.ui.theme.NekiTheme
+
+@Composable
+fun LoadingIndicator(
+    onDismissRequest: () -> Unit,
+    modifier: Modifier = Modifier,
+    circleColor: Color = NekiTheme.colorScheme.primary300,
+    backgroundColor: Color = NekiTheme.colorScheme.primary100,
+    properties: DialogProperties = DialogProperties(dismissOnBackPress = true),
+) {
+    Dialog(
+        onDismissRequest = onDismissRequest,
+        properties = properties,
+    ) {
+        CircularProgressIndicator(
+            modifier = modifier,
+            strokeWidth = 6.dp,
+            color = circleColor,
+            trackColor = backgroundColor,
+        )
+    }
+}
+
+@ComponentPreview
+@Composable
+private fun LoadingIndicatorPreview() {
+    NekiTheme {
+        LoadingIndicator(
+            onDismissRequest = {},
+        )
+    }
+}
