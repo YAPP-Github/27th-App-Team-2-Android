b18799545024def108029fd753b1121380922f37
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2026-01-22 20:55:19 +0900

    [feat] #33: Presigned URL 에 업로드 api 구현
    - `UploadService`를 사용하여 Presigned URL에 이미지를 업로드하는 `uploadImage` 함수를 `MediaUploadRepository`에 추가합니다.
    - 이에 따라 `MediaUploadRepositoryImpl`에 `UploadService` 의존성을 추가하고 `uploadImage` 함수를 구현합니다.

--- core/data-api/src/main/java/com/neki/android/core/dataapi/repository/MediaUploadRepository.kt
+++ core/data-api/src/main/java/com/neki/android/core/dataapi/repository/MediaUploadRepository.kt
@@ -1,8 +1,18 @@
 package com.neki.android.core.dataapi.repository
 
+import com.neki.android.core.model.ContentType
+import java.io.File
+
 interface MediaUploadRepository {
     suspend fun getUploadTicket(
         contentType: String,
         mediaType: String,
     ): Result<Pair<Long, String>>
+
+    suspend fun uploadImage(
+        fileName: String,
+        uploadUrl: String,
+        file: File,
+        contentType: ContentType,
+    ): Result<Unit>
 }
--- core/data/src/main/java/com/neki/android/core/data/repository/impl/MediaUploadRepositoryImpl.kt
+++ core/data/src/main/java/com/neki/android/core/data/repository/impl/MediaUploadRepositoryImpl.kt
@@ -1,13 +1,17 @@
 package com.neki.android.core.data.repository.impl
 
 import com.neki.android.core.data.remote.api.ApiService
+import com.neki.android.core.data.remote.api.UploadService
 import com.neki.android.core.data.remote.model.request.MediaUploadTicketRequest
 import com.neki.android.core.data.util.runSuspendCatching
 import com.neki.android.core.dataapi.repository.MediaUploadRepository
+import com.neki.android.core.model.ContentType
+import java.io.File
 import javax.inject.Inject
 
 class MediaUploadRepositoryImpl @Inject constructor(
     private val apiService: ApiService,
+    private val uploadService: UploadService,
 ) : MediaUploadRepository {
     override suspend fun getUploadTicket(
         contentType: String,
@@ -19,5 +23,19 @@ class MediaUploadRepositoryImpl @Inject constructor(
                 mediaType = mediaType,
             ),
         ).data.toPair()
+    }
+
+    override suspend fun uploadImage(
+        fileName: String,
+        uploadUrl: String,
+        file: File,
+        contentType: ContentType,
+    ) = runSuspendCatching {
+        uploadService.uploadImage(
+            fileName = fileName,
+            presignedUrl = uploadUrl,
+            imageFile = file,
+            contentType = contentType,
+        )
     }
 }
