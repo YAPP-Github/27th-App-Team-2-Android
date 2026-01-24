ebae33730083f58e3d3096188548aa7b41f545d1
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2026-01-24 19:08:23 +0900

    [feat] #46: 인생네컷 QR 이미지 URL 감지 로직 추가
    `PhotoWebViewClient`의 `shouldInterceptRequest` 메서드에 인생네컷 QR 코드 스캔 시 생성되는 이미지 URL을 감지하는 로직을 추가합니다.

--- feature/photo-upload/impl/src/main/java/com/neki/android/feature/photo_upload/impl/qrscan/util/PhotoWebViewClient.kt
+++ feature/photo-upload/impl/src/main/java/com/neki/android/feature/photo_upload/impl/qrscan/util/PhotoWebViewClient.kt
@@ -5,6 +5,7 @@ import android.webkit.WebResourceResponse
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import com.neki.android.feature.photo_upload.impl.BuildConfig
+import timber.log.Timber
 
 class PhotoWebViewClient(
     private val onImageUrlDetected: (String) -> Unit,
@@ -15,10 +16,20 @@ class PhotoWebViewClient(
         request: WebResourceRequest?,
     ): WebResourceResponse? {
         val url = request?.url.toString()
+        Timber.d(request?.url.toString())
 
-        // Photoism
-        if (url.contains(BuildConfig.PHOTOISM_URL) && url.endsWith(BuildConfig.PHOTOISM_IMG_URL_END)) {
-            onImageUrlDetected(url)
+        when {
+            // 포토이즘
+            url.contains(BuildConfig.PHOTOISM_URL) && url.endsWith(BuildConfig.PHOTOISM_IMG_URL_END) -> {
+                Timber.d("포토이즘 이미지")
+                onImageUrlDetected(url)
+            }
+
+            // 인생네컷
+            url.contains(BuildConfig.PHOTOISM_URL) && url.endsWith(BuildConfig.PHOTOISM_IMG_URL_END) -> {
+                Timber.d("인생네컷 이미지")
+                onImageUrlDetected(url)
+            }
         }
 
         return super.shouldInterceptRequest(view, request)
