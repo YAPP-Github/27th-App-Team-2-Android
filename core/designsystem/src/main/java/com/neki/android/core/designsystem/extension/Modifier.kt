e45aa3d6ba06768865c6d2a6d25ca5ba494d545c
Ojongseok <whdtjr6889@naver.com>
2026-01-24 14:22:06 +0900

    [design] #40 click 관련 확장함수 분리

--- core/designsystem/src/main/java/com/neki/android/core/designsystem/extension/Modifier.kt
+++ core/designsystem/src/main/java/com/neki/android/core/designsystem/extension/Modifier.kt
@@ -1,119 +1,14 @@
 package com.neki.android.core.designsystem.extension
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.ripple
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
-import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Paint
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.graphics.drawOutline
-import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.debugInspectorInfo
-import androidx.compose.ui.semantics.Role
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
-/**
- * 클릭의 리플 효과를 없애주는 [Modifier]
- * */
-inline fun Modifier.noRippleClickable(crossinline onClick: () -> Unit): Modifier = composed {
-    clickable(
-        indication = null,
-        interactionSource = remember { MutableInteractionSource() },
-    ) {
-        onClick()
-    }
-}
-
-/**
- * 클릭의 리플 효과를 없애고 500L 내에 중복 클릭을를 막아주는 [Modifier]
- * */
-fun Modifier.noRippleClickableSingle(
-    enabled: Boolean = true,
-    onClickLabel: String? = null,
-    role: Role? = null,
-    onClick: () -> Unit,
-) = composed(
-    inspectorInfo = debugInspectorInfo {
-        name = "noRippleClickableSingle"
-        properties["enabled"] = enabled
-        properties["onClickLabel"] = onClickLabel
-        properties["role"] = role
-        properties["onClick"] = onClick
-    },
-) {
-    val multipleEventsCutter = remember { MultipleEventsCutter.get() }
-    Modifier.clickable(
-        enabled = enabled,
-        onClickLabel = onClickLabel,
-        onClick = { multipleEventsCutter.processEvent { onClick() } },
-        role = role,
-        indication = null,
-        interactionSource = remember { MutableInteractionSource() },
-    )
-}
-
-/**
- * 500L 내의 중복 클릭을 막아주는 [Modifier]
- * */
-fun Modifier.clickableSingle(
-    enabled: Boolean = true,
-    onClickLabel: String? = null,
-    role: Role? = null,
-    onClick: () -> Unit,
-) = composed(
-    inspectorInfo = debugInspectorInfo {
-        name = "clickableSingle"
-        properties["enabled"] = enabled
-        properties["onClickLabel"] = onClickLabel
-        properties["role"] = role
-        properties["onClick"] = onClick
-    },
-) {
-    val multipleEventsCutter = remember { MultipleEventsCutter.get() }
-    Modifier.clickable(
-        enabled = enabled,
-        onClickLabel = onClickLabel,
-        onClick = { multipleEventsCutter.processEvent { onClick() } },
-        role = role,
-        indication = ripple(),
-        interactionSource = remember { MutableInteractionSource() },
-    )
-}
-
-internal interface MultipleEventsCutter {
-    fun processEvent(event: () -> Unit)
-
-    companion object
-}
-
-internal fun MultipleEventsCutter.Companion.get(): MultipleEventsCutter =
-    MultipleEventsCutterImpl()
-
-private class MultipleEventsCutterImpl : MultipleEventsCutter {
-    private val now: Long
-        get() = System.currentTimeMillis()
-
-    private var lastEventTimeMs: Long = 0
-
-    override fun processEvent(event: () -> Unit) {
-        if (now - lastEventTimeMs >= 500L) {
-            lastEventTimeMs = now
-            event.invoke()
-        }
-    }
-}
 /**
  * 사진 컴포넌트에 적용되는 그라데이션 배경
  * 좌하단에서 우상단으로 갈수록 어두워지는 효과
---
+++ core/designsystem/src/main/java/com/neki/android/core/designsystem/modifier/Clickable.kt
@@ -0,0 +1,101 @@
+package com.neki.android.core.designsystem.modifier
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.material3.ripple
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.platform.debugInspectorInfo
+import androidx.compose.ui.semantics.Role
+
+/**
+ * 클릭의 리플 효과를 없애주는 [Modifier]
+ * */
+inline fun Modifier.noRippleClickable(crossinline onClick: () -> Unit): Modifier = composed {
+    clickable(
+        indication = null,
+        interactionSource = remember { MutableInteractionSource() },
+    ) {
+        onClick()
+    }
+}
+
+/**
+ * 클릭의 리플 효과를 없애고 500L 내에 중복 클릭을를 막아주는 [Modifier]
+ * */
+fun Modifier.noRippleClickableSingle(
+    enabled: Boolean = true,
+    onClickLabel: String? = null,
+    role: Role? = null,
+    onClick: () -> Unit,
+) = composed(
+    inspectorInfo = debugInspectorInfo {
+        name = "noRippleClickableSingle"
+        properties["enabled"] = enabled
+        properties["onClickLabel"] = onClickLabel
+        properties["role"] = role
+        properties["onClick"] = onClick
+    },
+) {
+    val multipleEventsCutter = remember { MultipleEventsCutter.get() }
+    Modifier.clickable(
+        enabled = enabled,
+        onClickLabel = onClickLabel,
+        onClick = { multipleEventsCutter.processEvent { onClick() } },
+        role = role,
+        indication = null,
+        interactionSource = remember { MutableInteractionSource() },
+    )
+}
+
+/**
+ * 500L 내의 중복 클릭을 막아주는 [Modifier]
+ * */
+fun Modifier.clickableSingle(
+    enabled: Boolean = true,
+    onClickLabel: String? = null,
+    role: Role? = null,
+    onClick: () -> Unit,
+) = composed(
+    inspectorInfo = debugInspectorInfo {
+        name = "clickableSingle"
+        properties["enabled"] = enabled
+        properties["onClickLabel"] = onClickLabel
+        properties["role"] = role
+        properties["onClick"] = onClick
+    },
+) {
+    val multipleEventsCutter = remember { MultipleEventsCutter.get() }
+    Modifier.clickable(
+        enabled = enabled,
+        onClickLabel = onClickLabel,
+        onClick = { multipleEventsCutter.processEvent { onClick() } },
+        role = role,
+        indication = ripple(),
+        interactionSource = remember { MutableInteractionSource() },
+    )
+}
+
+internal interface MultipleEventsCutter {
+    fun processEvent(event: () -> Unit)
+
+    companion object
+}
+
+internal fun MultipleEventsCutter.Companion.get(): MultipleEventsCutter =
+    MultipleEventsCutterImpl()
+
+private class MultipleEventsCutterImpl : MultipleEventsCutter {
+    private val now: Long
+        get() = System.currentTimeMillis()
+
+    private var lastEventTimeMs: Long = 0
+
+    override fun processEvent(event: () -> Unit) {
+        if (now - lastEventTimeMs >= 500L) {
+            lastEventTimeMs = now
+            event.invoke()
+        }
+    }
+}
