f6ff2f6c03e44a12a86211b3cf1b359beed8f5d4
Ojongseok <whdtjr6889@naver.com>
2026-01-25 11:14:48 +0900

    [fix] #51 드래그 관련 높이 상수 네이밍 변경

--- feature/map/impl/src/main/java/com/neki/android/feature/map/impl/const/MapConst.kt
+++ feature/map/impl/src/main/java/com/neki/android/feature/map/impl/const/MapConst.kt
@@ -3,8 +3,8 @@ package com.neki.android.feature.map.impl.const
 internal object MapConst {
     internal const val BOTTOM_NAVIGATION_BAR_HEIGHT = 52
     internal const val PANEL_DRAG_LOCATION_HEIGHT = 48
-    internal const val PANEL_DRAG_LEVEL_BOTTOM_HEIGHT = 96
-    internal const val PANEL_DRAG_LEVEL_CENTER_HEIGHT = 218
+    internal const val PANEL_DRAG_LEVEL_FIRST_HEIGHT = 96
+    internal const val PANEL_DRAG_LEVEL_SECOND_HEIGHT = 218
 
     // 마커
     internal const val MARKER_BACKGROUND_RADIUS = 20
