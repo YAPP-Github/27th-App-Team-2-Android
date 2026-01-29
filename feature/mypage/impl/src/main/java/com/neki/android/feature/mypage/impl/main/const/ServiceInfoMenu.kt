953954da1bcad2c1b0ee8dd1052dcb5bffc02098
Ojongseok <whdtjr6889@naver.com>
2026-01-29 20:45:39 +0900

    [chore] #72 서비스 정보 관련 상수 정의

---
+++ feature/mypage/impl/src/main/java/com/neki/android/feature/mypage/impl/main/const/ServiceInfoMenu.kt
@@ -0,0 +1,23 @@
+package com.neki.android.feature.mypage.impl.main.const
+
+enum class ServiceInfoMenu(
+    val text: String,
+    val url: String,
+) {
+    INQUIRY(
+        text = "Neki에 문의하기",
+        url = "https://www.naver.com",
+    ),
+    TERMS_OF_SERVICE(
+        text = "이용약관",
+        url = "https://lydian-tip-26b.notion.site/2ee0d9441db0807c8684ce3e2d4b8aca?source=copy_link",
+    ),
+    PRIVACY_POLICY(
+        text = "개인정보 처리방침",
+        url = "https://www.naver.com",
+    ),
+    OPEN_SOURCE_LICENSE(
+        text = "오픈소스 라이선스",
+        url = "https://www.naver.com",
+    ),
+}
