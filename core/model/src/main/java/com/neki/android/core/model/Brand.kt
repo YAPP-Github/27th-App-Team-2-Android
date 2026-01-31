66c32bc2872549576934c325f52610a9dd489bf0
Ojongseok <whdtjr6889@naver.com>
2026-01-31 13:41:09 +0900

    [chore] #63 :core:model 내 UI에 표현되는 data class @Immutable 어노테이션 추가

--- core/model/src/main/java/com/neki/android/core/model/Brand.kt
+++ core/model/src/main/java/com/neki/android/core/model/Brand.kt
@@ -1,5 +1,8 @@
 package com.neki.android.core.model
 
+import androidx.compose.runtime.Immutable
+
+@Immutable
 data class Brand(
     val isChecked: Boolean = false,
     val id: Long = 0L,
--- core/model/src/main/java/com/neki/android/core/model/Pose.kt
+++ core/model/src/main/java/com/neki/android/core/model/Pose.kt
@@ -1,8 +1,10 @@
 package com.neki.android.core.model
 
+import androidx.compose.runtime.Immutable
 import kotlinx.serialization.Serializable
 
 @Serializable
+//@Immutable
 data class Pose(
     val id: Long = 0L,
     val poseImageUrl: String = "",
