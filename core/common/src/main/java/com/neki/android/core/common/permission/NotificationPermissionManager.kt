425fcd51ea3c50b5270a756292c6e1370ffc4549
Ojongseok <whdtjr6889@naver.com>
2026-02-06 07:24:03 +0900

    [fix] #79 알림 권한 확인 로직 버전별 분기 추가

--- core/common/src/main/java/com/neki/android/core/common/permission/NotificationPermissionManager.kt
+++ core/common/src/main/java/com/neki/android/core/common/permission/NotificationPermissionManager.kt
@@ -5,12 +5,16 @@ import android.app.Activity
 import android.content.Context
 import android.os.Build
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.PermissionChecker
 
 object NotificationPermissionManager {
     const val NOTIFICATION_PERMISSION = Manifest.permission.POST_NOTIFICATIONS
 
     fun isGrantedNotificationPermission(context: Context): Boolean {
-        return NotificationManagerCompat.from(context).areNotificationsEnabled()
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            ContextCompat.checkSelfPermission(context, NOTIFICATION_PERMISSION) == PermissionChecker.PERMISSION_GRANTED
+        } else NotificationManagerCompat.from(context).areNotificationsEnabled()
     }
 
     fun shouldShowNotificationRationale(activity: Activity): Boolean {
