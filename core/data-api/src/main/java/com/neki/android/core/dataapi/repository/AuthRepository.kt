f95ac0eb010816dcdcfcd69270f67d2591f79990
Ojongseok <whdtjr6889@naver.com>
2026-02-08 13:32:24 +0900

    [feat] #80 온보딩 노출 여부 확인 로직 추가

--- core/data-api/src/main/java/com/neki/android/core/dataapi/repository/AuthRepository.kt
+++ core/data-api/src/main/java/com/neki/android/core/dataapi/repository/AuthRepository.kt
@@ -1,9 +1,13 @@
 package com.neki.android.core.dataapi.repository
 
 import com.neki.android.core.model.Auth
+import kotlinx.coroutines.flow.Flow
 
 interface AuthRepository {
     suspend fun loginWithKakao(idToken: String): Result<Auth>
     suspend fun updateAccessToken(refreshToken: String): Result<Auth>
     suspend fun withdrawAccount(): Result<Unit>
+
+    fun isReadOnboarding(): Flow<Boolean>
+    suspend fun setReadOnboarding(value: Boolean)
 }
