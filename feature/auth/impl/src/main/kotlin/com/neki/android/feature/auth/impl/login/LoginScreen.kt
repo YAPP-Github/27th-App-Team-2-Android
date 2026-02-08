96bded308fbfa120015c31558e560d2f0a3d5fd0
Ojongseok <whdtjr6889@naver.com>
2026-02-08 20:23:28 +0900

    [fix] #91 LoginScreen의 uiState 관찰 구문 제거

--- feature/auth/impl/src/main/kotlin/com/neki/android/feature/auth/impl/login/LoginScreen.kt
+++ feature/auth/impl/src/main/kotlin/com/neki/android/feature/auth/impl/login/LoginScreen.kt
@@ -3,13 +3,10 @@ package com.neki.android.feature.auth.impl.login
 import android.widget.Toast
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.neki.android.core.common.kakao.KakaoAuthHelper
 import com.neki.android.core.designsystem.ComponentPreview
 import com.neki.android.core.designsystem.ui.theme.NekiTheme
@@ -24,7 +21,6 @@ internal fun LoginRoute(
     navigateToTerm: () -> Unit,
 ) {
     val context = LocalContext.current
-    val uiState by viewModel.store.uiState.collectAsStateWithLifecycle()
 
     viewModel.store.sideEffects.collectWithLifecycle { sideEffect ->
         when (sideEffect) {
