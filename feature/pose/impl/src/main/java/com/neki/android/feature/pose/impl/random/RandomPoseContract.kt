388e033387ae45682c7f940e562830d2a69463f0
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2026-02-09 00:31:00 +0900

    [refactor] #89: hasNextPost 기본값 수정

--- feature/pose/impl/src/main/java/com/neki/android/feature/pose/impl/random/RandomPoseContract.kt
+++ feature/pose/impl/src/main/java/com/neki/android/feature/pose/impl/random/RandomPoseContract.kt
@@ -10,7 +10,7 @@ data class RandomPoseUiState(
     val currentIndex: Int = 0,
     val poseList: ImmutableList<Pose> = persistentListOf(),
     val committedScraps: Map<Long, Boolean> = emptyMap(),
-    val hasNewPose: Boolean = false,
+    val hasNewPose: Boolean = true,
 ) {
     val currentPose: Pose?
         get() = poseList.getOrNull(currentIndex)
