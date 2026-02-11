a56c1c273069fdb4ffc3869270f494a410f78730
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2026-02-11 19:51:41 +0900

    [refactor] #95: NekiPermission enum을 core:common 모듈로 이동
    `feature:mypage` 모듈에 있던 `NekiPermission` enum 클래스를 여러 모듈에서 재사용할 수 있도록 `core:common` 모듈로 이동했습니다. 이에 따라 관련 import 구문들을 수정했습니다.

--- core/common/src/main/java/com/neki/android/core/common/permission/NekiPermission.kt
+++ core/common/src/main/java/com/neki/android/core/common/permission/NekiPermission.kt
@@ -1,4 +1,4 @@
-package com.neki.android.feature.mypage.impl.permission.const
+package com.neki.android.core.common.permission
 
 enum class NekiPermission(
     val title: String,
@@ -8,16 +8,16 @@ enum class NekiPermission(
     CAMERA(
         title = "카메라",
         subTitle = "QR 촬영에 필요해요.",
-        dialogContent = "카메라 권한을 허용해주세요.",
+        dialogContent = "QR 인식을 위해 카메라 접근이 필요해요",
     ),
     LOCATION(
         title = "위치",
         subTitle = "주변 포토부스 탐색에 필요해요.",
-        dialogContent = "위치 권한을 허용해주세요.",
+        dialogContent = "주변 포토부스를 찾기 위해 \n위치 사용 권한이 필요해요",
     ),
     NOTIFICATION(
         title = "알림",
         subTitle = "저장 사진 및 추억 리마인드에 필요해요.",
-        dialogContent = "알림 권한을 허용해주세요.",
+        dialogContent = "사진 저장 완료·오류 안내를 알려드리기 위해\n알림 권한이 필요해요",
     ),
 }
