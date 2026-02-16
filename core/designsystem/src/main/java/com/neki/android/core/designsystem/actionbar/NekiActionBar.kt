5917d82d9f50e886ee4c035b3753bdf7f266140b
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2026-02-16 22:26:22 +0900

    [design] #108: NekiActionBar 뒤로가기 아이콘 크기 조정
    `NekiActionBar` 컴포저블의 뒤로가기 버튼 아이콘(`icon_arrow_left`) 크기를 28dp로 설정하여 디자인을 수정했습니다.

--- core/designsystem/src/main/java/com/neki/android/core/designsystem/actionbar/NekiActionBar.kt
+++ core/designsystem/src/main/java/com/neki/android/core/designsystem/actionbar/NekiActionBar.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
@@ -100,6 +101,7 @@ private fun NekiStartActionBarPreview() {
                 onClick = {},
             ) {
                 Icon(
+                    modifier = Modifier.size(28.dp),
                     imageVector = ImageVector.vectorResource(R.drawable.icon_arrow_left),
                     contentDescription = null,
                     tint = NekiTheme.colorScheme.gray900,
