d1336dcef5c1d5623ede542613180ff60c011258
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2026-02-20 23:56:19 +0900

    [chore] #128: 사용하지 않는 import 제거
    `NekiTextButton.kt` 파일에서 사용되지 않는 `androidx.compose.ui.graphics.Color` import 문을 제거했습니다.

--- core/designsystem/src/main/java/com/neki/android/core/designsystem/button/NekiTextButton.kt
+++ core/designsystem/src/main/java/com/neki/android/core/designsystem/button/NekiTextButton.kt
@@ -7,7 +7,6 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import com.neki.android.core.designsystem.ComponentPreview
 import com.neki.android.core.designsystem.modifier.MultipleEventsCutter
 import com.neki.android.core.designsystem.modifier.get
