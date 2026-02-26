d1f12a250648a4c772ec0354b7c801836b618ceb
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2026-02-27 03:11:38 +0900

    [refactor] #154: Compose 런타임 최적화를 위해 ResultEventBus에 @Stable 적용
    `ResultEventBus` 클래스에 `@Stable` 어노테이션을 추가하여 Compose 런타임이 해당 클래스를 안정적인 것으로 인식하고 불필요한 리컴포지션을 줄이도록 했습니다.
    또한, `channelMap`의 구현을 `mutableMapOf`에서 `mutableStateMapOf`로 변경하여 상태 변경을 Compose가 올바르게 관찰할 수 있도록 수정했습니다.

--- core/navigation/src/main/java/com/neki/android/core/navigation/result/ResultEventBus.kt
+++ core/navigation/src/main/java/com/neki/android/core/navigation/result/ResultEventBus.kt
@@ -3,7 +3,9 @@ package com.neki.android.core.navigation.result
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ProvidableCompositionLocal
 import androidx.compose.runtime.ProvidedValue
+import androidx.compose.runtime.Stable
 import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.runtime.mutableStateMapOf
 import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.Channel.Factory.BUFFERED
@@ -33,8 +35,9 @@ object LocalResultEventBus {
  * It provides a solution for event based results.
  */
 // https://github.com/android/nav3-recipes/blob/main/app/src/main/java/com/example/nav3recipes/results/event/README.md
+@Stable
 class ResultEventBus {
-    val channelMap: MutableMap<String, Channel<Any?>> = mutableMapOf()
+    val channelMap: MutableMap<String, Channel<Any?>> = mutableStateMapOf()
 
     inline fun <reified T> getResultFlow(resultKey: String = T::class.toString()) =
         channelMap[resultKey]?.receiveAsFlow()
