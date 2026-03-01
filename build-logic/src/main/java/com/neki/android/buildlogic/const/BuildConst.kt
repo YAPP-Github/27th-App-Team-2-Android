da36c7adcd06c0e9456fb8d20e69e7eed6ed672a
오종석 <98886487+Ojongseok@users.noreply.github.com>
2026-03-01 12:50:17 +0900

    Revert "[release] v.11 (1.1.0)"

--- build-logic/src/main/java/com/neki/android/buildlogic/const/BuildConst.kt
+++ build-logic/src/main/java/com/neki/android/buildlogic/const/BuildConst.kt
@@ -5,7 +5,7 @@ import org.gradle.api.JavaVersion
 object BuildConst {
     const val APPLICATION_ID = "com.neki.android"
 
-    const val VERSION_CODE = 11
+    const val VERSION_CODE = 10
     const val VERSION_NAME = "1.1.0"
 
     const val MIN_SDK = 29
