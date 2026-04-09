69fa667eccbb6aaf56c328a429708bac1fb2593b
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2026-04-09 23:57:26 +0900

    [feat] #178: select-album 모듈 생성 및 의존성 설정
    - feature:select-album (api, impl) 모듈 신규 생성
    - app 모듈 및 settings.gradle.kts에 신규 모듈 의존성 추가

---
+++ feature/select-album/impl/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    alias(libs.plugins.neki.android.feature.impl)
+}
+
+android {
+    namespace = "com.neki.android.feature.select_album.impl"
+}
+
+dependencies {
+    implementation(projects.feature.selectAlbum.api)
+}
