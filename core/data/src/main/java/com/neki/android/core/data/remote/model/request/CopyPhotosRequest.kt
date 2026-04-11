155d32240ecfaf3ad01d4d04bba08988b5ed30d7
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2026-04-12 05:24:32 +0900

    [chore]: MovePhotosRequest / CopyPhotosRequest 모델 추가

---
+++ core/data/src/main/java/com/neki/android/core/data/remote/model/request/CopyPhotosRequest.kt
@@ -0,0 +1,11 @@
+package com.neki.android.core.data.remote.model.request
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class CopyPhotosRequest(
+    @SerialName("sourceFolderId") val sourceFolderId: Long?,
+    @SerialName("photoIds") val photoIds: List<Long>,
+    @SerialName("targetFolderIds") val targetFolderIds: List<Long>,
+)
