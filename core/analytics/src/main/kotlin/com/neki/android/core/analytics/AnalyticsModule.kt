1301d2efa153145be476599f8411b79c7986fb9b
Ojongseok <whdtjr6889@naver.com>
2026-04-17 21:34:26 +0900

    [chore] #192 AnalyticsModule 생성

---
+++ core/analytics/src/main/kotlin/com/neki/android/core/analytics/AnalyticsModule.kt
@@ -0,0 +1,28 @@
+package com.neki.android.core.analytics
+
+import android.content.Context
+import com.google.firebase.analytics.FirebaseAnalytics
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal abstract class AnalyticsModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindAnalyticsLogger(impl: FirebaseAnalyticsLogger): AnalyticsLogger
+
+    companion object {
+        @Provides
+        @Singleton
+        fun provideFirebaseAnalytics(
+            @ApplicationContext context: Context,
+        ): FirebaseAnalytics = FirebaseAnalytics.getInstance(context)
+    }
+}
